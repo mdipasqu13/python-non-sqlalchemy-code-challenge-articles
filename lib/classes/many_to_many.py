87aed940da6d6f790dcf7a8c86fbc4de93fd1808
Michael DiPasquale <michaeldipasquale313@gmail.com>
2024-04-09 17:34:31 -0400

    completed, need to clean up and add comments

--- lib/classes/many_to_many.py
+++ lib/classes/many_to_many.py
@@ -1,38 +1,128 @@
 class Article:
+    
+    all = []
+    
     def __init__(self, author, magazine, title):
         self.author = author
         self.magazine = magazine
         self.title = title
+        Article.all.append(self)
         
+    @property
+    def author(self):
+        return self._author
+    
+    @author.setter
+    def author(self, new_author):
+        if isinstance(new_author, Author):
+        # and len(new_author) > 0 and not hasattr(self, "name)"):
+        # if type(new_author) == str and len(new_author) > 0 and not hasattr(self, "name"):
+            self._author = new_author
+            
+    @property
+    def magazine(self):
+        return self._magazine
+    
+    @magazine.setter
+    def magazine(self, new_magazine):
+        if isinstance(new_magazine, Magazine):
+            # if 2 <= len(new_magazine) <= 16:
+            self._magazine = new_magazine
+                
+    @property
+    def title(self):
+        return self._title
+    
+    @title.setter
+    def title(self, new_title):
+        if not hasattr(self, "title"):
+            if isinstance(new_title, str):
+                if 5<= len(new_title) <= 50:
+                    self._title = new_title
+    
+                    
 class Author:
     def __init__(self, name):
         self.name = name
 
+    @property
+    def name(self):
+        return self._name
+    
+    @name.setter
+    def name(self, new_name):
+        if not hasattr(self, "name"):
+            if isinstance(new_name, str):
+                self._name = new_name
+
     def articles(self):
-        pass
+        return [article for article in Article.all if self == article.author]
 
     def magazines(self):
-        pass
+        return list({article.magazine for article in self.articles()})
+
 
     def add_article(self, magazine, title):
-        pass
+        return Article(self, magazine, title)
 
     def topic_areas(self):
-        pass
+        topic_areas = list({magazine.category for magazine in self.magazines()})
+        if topic_areas:
+            return topic_areas
+        else:
+            return None
+        
 
 class Magazine:
     def __init__(self, name, category):
         self.name = name
         self.category = category
-
+    
+    @property
+    def name(self):
+        return self._name
+    
+    @name.setter
+    def name(self, new_name):
+        if isinstance(new_name, str) and 2 <= len(new_name) <= 16:
+            self._name = new_name
+    
+    @property
+    def category(self):
+        return self._category
+    
+    @category.setter
+    def category(self, new_category):
+        if isinstance(new_category, str):
+            if len(new_category):
+                self._category = new_category
+        
+    
     def articles(self):
-        pass
+        return [article for article in Article.all if self == article.magazine]
 
     def contributors(self):
-        pass
+        return list({article.author for article in self.articles()})
 
     def article_titles(self):
-        pass
+        article_titles = [magazine.title for magazine in self.articles()]
+        if article_titles:
+            return article_titles
+        else:
+            return None
 
     def contributing_authors(self):
-        pass
+        authors= {}
+        list_of_authors = []
+        for article in self.articles():
+            if article.author in authors:
+                authors[article.author] += 1
+            else: 
+                authors[article.author] = 1  
+        for author in authors:
+            if authors[author] >= 2:
+                list_of_authors.append(author)
+        if (list_of_authors):
+            return list_of_authors
+        else:
+            return None
